284613cac79739cb9a8fd2a2362d1c8034a25802
Owen Lian <owenlian88@gmail.com>
2024-05-19 19:38:15 -0400

    initial commit

---
+++ calculator.js
@@ -0,0 +1,37 @@
+console.log(operate(1,2,'*'))
+
+let first;
+let operator;
+let second;
+
+console.log(add(12,3))
+
+
+function add (a,b){
+    return a + b 
+}
+
+function substract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(left, right, operator)
+{
+    if(operator === '+')
+        return add(left,right);    
+    if(operator === '-')
+        return substract(left,right);   
+    if(operator === '*')
+        return multiply(left,right);    
+    if(operator === '/')
+        return divide(left,right);
+}
+
